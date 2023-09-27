195be3db703c78516f2827d9e53e2631c7ea2294
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2023-09-26 19:47:54 -0500

    Added java solution for Longest Consecutive Sequence

---
+++ LongestConsecutiveSequence/LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int count = 1;
+        int result = 1;
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        ArrayList<Integer> arr_list = new ArrayList<>(set);
+        Collections.sort(arr_list);
+
+        for (int i = 0; i < arr_list.size() - 1; i++) {
+            if (arr_list.get(i + 1) - arr_list.get(i) == 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            result = Math.max(count, result);
+        }
+
+        return result;
+    }
+}
