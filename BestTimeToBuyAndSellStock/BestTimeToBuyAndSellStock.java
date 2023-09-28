4793a800cef8a082fbfad7c4389fe457dfe06479
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2023-09-28 01:09:42 -0500

    Added java and python solution for Best Time to Buy and Sell Stock

---
+++ BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                result = Math.max(result, prices[i] - min);
+            }
+        }
+
+        return result;
+
+    }
+}
---
+++ BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,19 @@
+import sys
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        result = 0
+        min_value = sys.maxsize
+
+        for price in prices:
+            if price < min_value:
+                min_value = price
+            else:
+                result = max(result, price - min_value)
+
+        return result
