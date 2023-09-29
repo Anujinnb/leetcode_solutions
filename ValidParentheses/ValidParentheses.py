bad480574f1957b50be7166297c8c493c3382315
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2023-09-29 14:45:19 -0500

    Added java and python solution for Valid Parentheses

---
+++ ValidParentheses/ValidParentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for ch in s:
+            if ch in '([{':
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+
+                if ch == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif ch == ']' and stack[-1] == '[':
+                    stack.pop()
+                elif ch == '}' and stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
+
+        return not stack
