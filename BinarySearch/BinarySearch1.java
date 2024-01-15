a79b3caa320c912d160a51556b5fe249d343b7b0
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-01-14 18:41:03 -0600

    Added java solution with O(n) runtime for Binary Search

---
+++ BinarySearch/BinarySearch1.java
@@ -0,0 +1,13 @@
+// This solution gives O(n) runtime complexity
+
+class Solution {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
