515782f4f3d7ade704658f930d0c2ca719fc90bb
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-01-15 20:03:39 -0600

    Changed code for Single Number

--- SingleNumber/SingleNumber.py
+++ SingleNumber/SingleNumber.py
@@ -2,7 +2,7 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
 
-        for i in nums:
-            result = result ^ i
+        for num in nums:
+            result = result ^ num
 
         return result
