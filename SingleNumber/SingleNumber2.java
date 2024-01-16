eb967faf3fc04364e27707de3b7d53bf3f00c1f5
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-01-15 19:57:05 -0600

    Added java solution with XOR operation for Single Number

---
+++ SingleNumber/SingleNumber2.java
@@ -0,0 +1,13 @@
+//This solution uses bitwise XOR operation
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+
+        return result;
+    }
+}
