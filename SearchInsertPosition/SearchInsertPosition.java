72707e948b6063c9276ba720c056e6d9a0888296
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-01-17 20:11:13 -0600

    Added explanation for Search Insert Position (java)

--- SearchInsertPosition/SearchInsertPosition.java
+++ SearchInsertPosition/SearchInsertPosition.java
@@ -1,4 +1,10 @@
 //This code provides time complexity of O(log n)
+//Explanation for the code below: 
+//Since this problem should have time complexity of O(log n). So I achieved the result using binary search, which divided the array in half.
+//First of all, we get the indices of first number and last number. Then, we can find the middle element of these two indices. 
+//After that, we can compare the middle element with target element. If the target element is smaller than middle one, then search the left array.
+//If the target element is bigger than middle element, then the target element should be in the upper half of the whole array. 
+//If the target is equal to middle, then we should return that index.
 
 class Solution {
     public int searchInsert(int[] nums, int target) {
