e59da65ad73d41157dcb99d57bbe16977b3c9091
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-01-26 10:04:05 -0600

    Added: explanation for Delete Duplicate Emails

--- DeleteDuplicateEmails/DeleteDuplicateEmails.sql
+++ DeleteDuplicateEmails/DeleteDuplicateEmails.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
+#If IDs are different and the emails are equal, then we delete that person.
 DELETE p1 from Person p1, Person p2
 WHERE p1.email = p2.email and p1.id > p2.id
