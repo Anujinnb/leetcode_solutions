28aeaaeb2036751ce83f3a347171c8ba62f387c1
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-02-02 22:57:54 -0600

    Chore: add explanation for Minimum Depth of Binary Tree

--- MinDepthOfBinaryTree/MinDepthOfBinaryTree.java
+++ MinDepthOfBinaryTree/MinDepthOfBinaryTree.java
@@ -1,3 +1,8 @@
+//Explanation for the code: First we need to check whether the root is null or not. If it is null, then the depth is 0.
+//Otherwise, we can use recursion to find the depth of both left and right side of tree. If either one of them is null,
+//then minimum depth of the tree is the maximum of the depths of the non-null subtree plus . If that is not the case, 
+//then we can simply find the minimum depth by comparing the two.
+
 class Solution {
     public int minDepth(TreeNode root) {
         int result = 0;
