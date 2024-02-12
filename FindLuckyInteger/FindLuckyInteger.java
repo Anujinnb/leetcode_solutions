d9902bf4a4a2f3e5ee47682fde05aad7943ea9bc
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-02-11 23:21:26 -0600

    Chore: add explanation for problem Find Lucky Integer in an Array

--- FindLuckyInteger/FindLuckyInteger.java
+++ FindLuckyInteger/FindLuckyInteger.java
@@ -1,3 +1,10 @@
+//This solution gives time complexity of O(n) since it used the hashmap. 
+//Explanation: We need to iterate through the array first to check if the number exists in the hashmap or not.
+//If it already exists in the hashmap, then we store two things: the number, the count of occurrence. Otherwise, 
+//we just count that as one. On the next step, using entrySet, we check if the number equals the count of occurrence.
+//If that is the case, then we found the lucky number. Also, if there are multiple numbers that equals the count of occurrence,
+//then we get the highest number.
+
 class Solution {
     public int findLucky(int[] arr) {
         HashMap<Integer, Integer> map = new HashMap<>();
