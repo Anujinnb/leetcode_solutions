05382e291a979775774f54a3915aad6ce9af7042
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-02-16 23:22:45 -0600

    Chore: add explanation for Find Subarrays with Equal Sum

--- FindSubArraysWithEqualSum/FindSubArraysWithEqualSum.java
+++ FindSubArraysWithEqualSum/FindSubArraysWithEqualSum.java
@@ -1,3 +1,8 @@
+//Explanation for the code: we loop through the array. The current element plus next element (sum)
+//is added to the set if it is not present in the hashset. !set.add(sum) -- this checks whether the sum is already
+//present in the hashset. Since hashset doesn't allow duplicates, if the sum already exists in the set, then we found the
+//equal sum. So, we can return true.
+
 class Solution {
     public boolean findSubarrays(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
