c6f3bc2c4a078d89bd887200088af01a630e7f66
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-02-17 23:30:10 -0600

    Added: explanation for Valid Anagram

--- ValidAnagram/ValidAnagram2.java
+++ ValidAnagram/ValidAnagram2.java
@@ -1,4 +1,6 @@
-//This solution provides the runtime complexity of O(nlogn). The space complexity of this code is O(2n) ==> O(n)
+//This solution provides the runtime complexity of O(nlogn). The space complexity of this code is O(2n) ==> O(n).
+//Explanation: First, we need to make two strings into arrays of characters. Then, we sort those arrays of characters.
+//Then, after it is sorted, we can compare two arrays. If the arrays are equal, then it is anagram.
 
 class Solution {
     public boolean isAnagram(String s, String t) {
