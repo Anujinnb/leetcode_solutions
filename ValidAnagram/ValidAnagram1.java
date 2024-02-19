22cca5fee47a51f2f76dab4ceffa7602ae43f663
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-02-18 23:47:18 -0600

    Added: explanation for Valid Anagram

--- ValidAnagram/ValidAnagram1.java
+++ ValidAnagram/ValidAnagram1.java
@@ -1,4 +1,8 @@
-//This solution provides runtime complexity of O(n)
+//This solution provides runtime complexity of O(n).
+//Explanation: Comparing the hashmaps of the two strings, we can check whether it is anagram or not.
+//First, we create hashmap for string s. We check if each character in the string exists in the hashmap. If it exists, then
+//we increment the count of that character. If it doesn't exist, then we add the character alongside the count of 1 (occurrence).
+//We do the same thing for string t. Finally, we can compare all the items inside the hashmaps.
 
 class Solution {
     public boolean isAnagram(String s, String t) {
