043c307f3627d21d77e4e2327380dffc9243fc11
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-02-19 23:32:11 -0600

    Added: explanation for Single Number

--- SingleNumber/SingleNumber1.java
+++ SingleNumber/SingleNumber1.java
@@ -1,4 +1,7 @@
-//This code provides hashmap solution with runtime complexity of O(n) and
+//This code provides hashmap solution with runtime complexity of O(n). The space complexity for this code is also O(n).
+//Explanation: First, we need to check whether the number in the nums array exists in the hashmap. If it exists, then we increment
+//the count of occurrence alongside the number. Otherwise, we put the number and count of 1 (since its first occurrence) in the hashmap.
+//Finally, using the entrySet, if the value (number of occurrence) equals to 1, then we find that single number in the array.
 
 class Solution {
     public int singleNumber(int[] nums) {
