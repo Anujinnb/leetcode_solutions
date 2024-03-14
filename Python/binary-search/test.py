8030dfa583d55ad8c470944e919ce9a88313d366
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-03-13 22:44:41 -0500

    Updated: test cases for Binary Search

--- Python/binary-search/test.py
+++ Python/binary-search/test.py
@@ -1,10 +1,22 @@
 from solution import Solution
-from typing import List
 
-def run_test_cases():
-    # Test Case 1
-    nums: List[int] = [-1, 0, 3, 5, 9, 12]
-    target: int = 9
-    assert Solution().bruteforce_search(nums, target) == 4
+# Test cases for bruteforce_search
+def test_bruteforce_search():
+    sol = Solution()
+    assert sol.bruteforce_search([-1, 0, 3, 5, 9, 12], 9) == 4
+    assert sol.bruteforce_search([-1, 0, 3, 5, 9, 12], 2) == -1
+    assert sol.bruteforce_search([], 5) == -1
+    print("Bruteforce solution test cases passed successfully!")
 
-    print("All test cases passed successfully!")
+# Test cases for binary_search
+def test_binary_search():
+    sol = Solution()
+    assert sol.bruteforce_search([-1, 0, 3, 5, 9, 12], 9) == 4
+    assert sol.bruteforce_search([-1, 0, 3, 5, 9, 12], 2) == -1
+    assert sol.bruteforce_search([], 5) == -1
+    print("Binary search solution test cases passed successfully!")
+
+if __name__ == "__main__":
+    test_bruteforce_search()
+    test_binary_search()
+    print("All test cases passed!")
