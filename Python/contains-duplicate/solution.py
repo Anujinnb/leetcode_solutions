7b857e46727c72f2958d115f87d87ba4eff991c4
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-03-15 15:25:23 -0500

    Added: multiple solutions for Contains Duplicate

--- Python/contains-duplicate/solution.py
+++ Python/contains-duplicate/solution.py
@@ -1,16 +1,39 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-
+class Solution:
+    def containsDuplicate_bruteforce(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        
+        return False
+    
+    def containsDuplicate_sort(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        
+        return False
+    
+    def containsDuplicate_hashset(self, nums: List[int]) -> bool:
         hashset = set()
 
-        for i in nums:
-            if i in hashset:
+        for num in nums:
+            if num in hashset:
                 return True
-            else:
-                hashset.add(i)
+            
+            hashset.add(num)
+        
+        return False
+
+    def containsDuplicate_hashmap(self, nums: List[int]) -> bool:
+        hashmap = {}
+
+        for key, value in enumerate(nums):
+            if value in hashmap:
+                return True
+            
+            hashmap[value] = key
 
         return False
+        
