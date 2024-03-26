328f8d0a76da117c4a06358cc0344e901dd60487
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-03-26 13:14:05 -0500

    Added brute force java solution for Contains Duplicate

--- Java/contains-duplicate/Solution1.java
+++ Java/contains-duplicate/Solution1.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       HashMap<Integer, Integer> map = new HashMap<>();
-
-       for (int i = 0; i < nums.length; i++) {
-        if (map.containsKey(nums[i])) {
-            return true;
-        } else {
-            map.put(nums[i], i);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
         }
-       }
-       return false;
+
+        return false;
     }
 }
