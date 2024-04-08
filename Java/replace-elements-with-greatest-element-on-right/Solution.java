35dea7c8e63a9f6aaaec22fae74062afff277081
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-04-07 23:39:27 -0500

    Added time complexity for Replace Elements with Greatest Element on Right Side

--- Java/replace-elements-with-greatest-element-on-right/Solution.java
+++ Java/replace-elements-with-greatest-element-on-right/Solution.java
@@ -1,3 +1,5 @@
+//Time complexity of this program is O(n) since we loop through the array once. The space complexity is O(1).
+
 class Solution {
     public int[] replaceElements(int[] arr) {
         int max = -1;
