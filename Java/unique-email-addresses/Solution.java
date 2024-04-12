4d8a36dcd730022e862e1085ec69d8dce0d3ea10
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-04-12 16:28:11 -0500

    Added java solution for Unique Email Addresses

---
+++ Java/unique-email-addresses/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        HashSet<String> set = new HashSet<>();
+
+        for (String email : emails) {
+            String[] names = email.split("\\@");
+            String local = names[0];
+            String domain = names[1];
+
+            if (local.contains(".")) {
+                local = local.replace(".", "");
+            }
+
+            int plus_index = local.indexOf("+");
+
+            if (plus_index != -1) {
+                local = local.substring(0, plus_index);
+            }
+
+            String processedEmail = local + "@" + domain;
+
+            if (!set.contains(processedEmail)) {
+                set.add(processedEmail);
+            }
+        }
+
+        return set.size();
+    }
+}
