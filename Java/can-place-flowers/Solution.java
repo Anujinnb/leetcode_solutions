e5775c33c50ac32fb5c60d12c3f6c94f2715c6d1
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-04-18 18:09:16 -0500

    Added java explanation for Can Place Flowers

--- Java/can-place-flowers/Solution.java
+++ Java/can-place-flowers/Solution.java
@@ -1,4 +1,9 @@
-//Runtime complexity: O (n) because we loop through the flowerbed array once
+//Runtime complexity: O(n) because we loop through the flowerbed array once
+//Space complexity: O(1) because regardless of the input it uses constant space
+//Explanation: First check if n is 0 then we don't need to do anything so return true. Otherwise, we loop through the flowerbed array 
+//and check if we can place flowers at current position. In this case, if it is 0, then we check the two sides. If both previous and next positions
+//are available, then we can place the flower, which is assigning the value to 1 and reduce n. If we place all the flowers (n == 0), then we can
+//simply return true.  
 
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
