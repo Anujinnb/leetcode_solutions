081e5933bb0dc04f86c1fed371f1eae99eed6b3d
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-04-18 18:10:34 -0500

    Added python explanation for Can Place Flowers

--- Python/can-place-flowers/solution.py
+++ Python/can-place-flowers/solution.py
@@ -1,3 +1,8 @@
+# Explanation: First check if n is 0 then we don't need to do anything so return true. Otherwise, we loop through the flowerbed array 
+# and check if we can place flowers at current position. In this case, if it is 0, then we check the two sides. If both previous and next positions
+# are available, then we can place the flower, which is assigning the value to 1 and reduce n. If we place all the flowers (n == 0), then we can
+# simply return true.  
+
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         if n == 0:
