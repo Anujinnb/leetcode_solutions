e1be8782b70b163fced9415d1e71f59650dfc578
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-05-07 15:00:03 -0500

    Added java solution for Best Time to Buy and Sell Stock II

--- Java/best-time-to-buy-and-sell-stock-II/Solution2.java
+++ Java/best-time-to-buy-and-sell-stock-II/Solution2.java
@@ -1,18 +1,14 @@
-//Using sliding window technique (2 pointers)
-
 class Solution {
     public int maxProfit(int[] prices) {
         int left = 0;
         int result = 0;
 
         for (int right = 0; right < prices.length; right++) {
-            while (prices[left] > prices[right]) {
-                left = right;
+            if (prices[left] < prices[right]) {
+                result += prices[right] - prices[left];
             }
 
-            if (prices[left] < prices[right]) {
-                result = Math.max(result, prices[right] - prices[left]);
-            }
+            left = right;
         }
 
         return result;
