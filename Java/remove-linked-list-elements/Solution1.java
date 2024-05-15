24d41c82dc6b09bc1b84833d8dcfb47381d7ec55
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-05-14 22:01:22 -0500

    Added explanation for Remove Linked List Elements

--- Java/remove-linked-list-elements/Solution1.java
+++ Java/remove-linked-list-elements/Solution1.java
@@ -1,3 +1,7 @@
+//Explanation: If the current head itself equals to the value, then we remove that value from the linked list. Then,
+//using the same method from Remove Duplicates from Linked List, if the next node of the current equals to the value, then we remove that next node,
+//Otherwise, we can just move the current to the next node.
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         while (head != null && head.val == val) {
