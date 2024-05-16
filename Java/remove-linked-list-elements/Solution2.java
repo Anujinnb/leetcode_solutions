2b67574bc1048a9170a210cb023fde1549581da2
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-05-15 22:09:00 -0500

    Added explanation for Remove Linked List Elements

--- Java/remove-linked-list-elements/Solution2.java
+++ Java/remove-linked-list-elements/Solution2.java
@@ -1,13 +1,7 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+//Explanation: First, we check if the head is null or not. Then we initialize dummy node and connect it to the head. We also have prev node
+//that points to the dummy and current node that points to the head. If current node's value equals to the value, then prev node's next node is
+//going to be current's next node. Otherwise, then we move prev node to the current. 
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) {
