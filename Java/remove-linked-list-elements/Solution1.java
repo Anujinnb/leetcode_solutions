54f86084d7c18161cb794d3a5c9e6d5a718ff90a
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-05-16 22:13:16 -0500

    Added runtime and space complexity for Remove Linked List Elements

--- Java/remove-linked-list-elements/Solution1.java
+++ Java/remove-linked-list-elements/Solution1.java
@@ -2,6 +2,9 @@
 //using the same method from Remove Duplicates from Linked List, if the next node of the current equals to the value, then we remove that next node,
 //Otherwise, we can just move the current to the next node.
 
+//Runtime Complexity: O(n) we traverse through the linked list once (where each node is visited only once)
+//Space Complexity: O(1) current and head are not dependent on the size of the linked list
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         while (head != null && head.val == val) {
