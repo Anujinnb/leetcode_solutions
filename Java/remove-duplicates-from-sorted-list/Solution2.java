e78a9974aa92a51a0a325bee999c196f648d75cf
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-05-19 22:23:30 -0500

    Added explanation for Remove Duplicates from Linked List

--- Java/remove-duplicates-from-sorted-list/Solution2.java
+++ Java/remove-duplicates-from-sorted-list/Solution2.java
@@ -1,6 +1,9 @@
 //Runtime complexity: O(n) we traverse through the linked list only once
 //Space complexity: O(1) pointers don't require extra space
 
+//Explanation: First, we check if current node is null or not. While current and next of the current node are not null, then we check if next node
+//the value. While current node's value and next node's value are equal, then we move the next node pointer to the next.
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode current = head;
