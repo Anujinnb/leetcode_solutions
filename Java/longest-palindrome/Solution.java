5b8cd5c359bb461cc1463b3cb341e0cdb9088bc0
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-05-22 14:20:52 -0500

    Added explanation for Longest Palindrome

--- Java/longest-palindrome/Solution.java
+++ Java/longest-palindrome/Solution.java
@@ -1,3 +1,7 @@
+//Explanation: First, check if string is empty or not. Using hashset, if character doesn't exist in the hashset, we add to the hashset.
+//If character does exist in the hashset, then we remove that character from the set and increment the result by 2 because we assume that 
+//we use that character twice. Finally, if the set is not empty (the characters that has count of 1), then we simply add that one to the result.
+
 class Solution {
     public int longestPalindrome(String s) {
         if (s.isEmpty()) {
