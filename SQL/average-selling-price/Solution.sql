97bc7867900a8e4f12fb3c09cc9ef74843a5a320
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-06-18 11:50:08 -0500

    Fixed Average Selling Price solution

--- SQL/average-selling-price/Solution.sql
+++ SQL/average-selling-price/Solution.sql
@@ -1,4 +1,4 @@
-SELECT p.product_id, ROUND(SUM(units * price) / SUM(units), 2) as average_price FROM Prices p
-JOIN UnitsSold u
+SELECT p.product_id, IFNULL(ROUND(SUM(units * price) / SUM(units), 2), 0) as average_price FROM Prices p
+LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id AND u.purchase_date BETWEEN start_date AND end_date
 GROUP BY p.product_id
