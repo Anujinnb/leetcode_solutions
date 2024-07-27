cdf14a1da8dac810854559f09449b857099c2bae
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-07-26 20:06:09 -0500

    Add explanation for Valid Palindrome

--- Python/valid-palindrome/solution.py
+++ Python/valid-palindrome/solution.py
@@ -1,6 +1,10 @@
 # The runtime complexity of this program is O(n) because we traverse through the string once.
 # Space complexity is O(n) due to the creation of new strings during the lowercase conversion and removal of non-alphanumeric characters.
 
+# Explanation: First, we check if string is empty or not. If string is empty, we just return true. Then, we convert the string to lowercase and remove any non-alphanumeric characters.
+# Then we use two pointers: one starts at from the beginning of string, another one starts from the end of string. We compare the element at each pointer. If two characters don't match,
+# we just return false. Otherwise, we traverse until two pointers meet in the middle.
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         if not s:
