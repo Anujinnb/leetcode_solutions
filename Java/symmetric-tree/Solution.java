60fd06d9fe6d557c74d6d4512c5e9c0cf292c07b
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-07-31 18:46:14 -0500

    Add time and space complexity for Symmetric Tree

--- Java/symmetric-tree/Solution.java
+++ Java/symmetric-tree/Solution.java
@@ -1,3 +1,6 @@
+//Space complexity of this program: 1. If the tree is balanced, the best case is O(logn) 2. If the tree is skewed, the worst case is O(n).
+//Time complexity: O(n) because each node is visited only once.
+
 class Solution {
     public boolean checkSymmetry(TreeNode node1, TreeNode node2) {
         if (node1 == null && node2 == null) {
