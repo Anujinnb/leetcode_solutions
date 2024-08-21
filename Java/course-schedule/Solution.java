e41b65bf3dab0035809a814c2f465d5c66ca07dd
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-08-21 14:20:52 -0500

    Added java solution for Course Schedule

---
+++ Java/course-schedule/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            int src = prerequisite[1];
+            int dest = prerequisite[0];
+            indegree[dest]++;
+            adj.get(src).add(dest);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int neighbor : adj.get(node)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+
+        for (int i : indegree) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
