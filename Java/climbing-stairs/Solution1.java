9bc46d83c3a04b945c4657c143d644de31d8e047
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-08-23 15:05:39 -0500

    Added time and space complexity explanation for Climbing Stairs

--- Java/climbing-stairs/Solution1.java
+++ Java/climbing-stairs/Solution1.java
@@ -1,3 +1,7 @@
+//1. Time complexity is O(n). The for-loop runs from i = 2 to i = n. 
+//This loop executes n - 1 times, which is proportional to n.
+//2. The space required is proportional to n, leading to a space complexity of O(n).
+
 class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1) {
