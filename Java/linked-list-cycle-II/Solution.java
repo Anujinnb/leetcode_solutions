c03855a8dedca286dafad7d3f92b8cd435b320b5
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-09-18 13:04:26 -0500

    Added runtime and space complexity for Linked List Cycle II

--- Java/linked-list-cycle-II/Solution.java
+++ Java/linked-list-cycle-II/Solution.java
@@ -1,3 +1,10 @@
+//1. The total time complexity is O(n), where n is the number of nodes in the list, because:
+// The first while loop takes O(n) time in the worst case.
+// The second while loop also takes O(n) time.
+// So, the total time complexity is O(n).
+
+//2. The space complexity is O(1) because we are only using a fixed amount of extra space regardless of the size of the input list.
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
