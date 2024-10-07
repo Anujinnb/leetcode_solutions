13abc30f7ae71b5812998aad343e7d9e1095679f
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-10-07 17:09:47 -0500

    Added time and space complexity explanation for Same Tree

--- Java/same-tree/Solution2.java
+++ Java/same-tree/Solution2.java
@@ -1,3 +1,8 @@
+// The time complexity of this program is O(n), where n is the total number of nodes in the larger of the two trees.
+// The space complexity is O(h) where h is the height of the larger of two trees
+// 1. Balanced Tree: the height h = O(log n)
+// 2. Skewed Tree: h = O(n)
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
