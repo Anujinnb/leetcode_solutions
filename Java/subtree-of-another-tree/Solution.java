2e94417480c7e9aae69c5bbc5bd3887f5f971bf2
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-10-08 18:02:16 -0500

    Added java solution for Subtree of Another Tree

---
+++ Java/subtree-of-another-tree/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+
+        return p.val == q.val && left && right;
+    }
+}
