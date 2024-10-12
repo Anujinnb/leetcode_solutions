0a5cb3ef542dc0aaa30668f9f3c621c7315c9e8c
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-10-12 11:34:27 -0500

    Added java solution for Lowest Common Ancestor of Deepest Leaves

---
+++ Java/lowest-common-ancestor-of-deepest-leaves/Solution1.java
@@ -0,0 +1,31 @@
+//This is a leetcode version of the problem
+
+class Solution {
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if (left == right) {
+            return root;
+        } else if (left > right) {
+            return lcaDeepestLeaves(root.left);
+        } else {
+            return lcaDeepestLeaves(root.right);
+        }
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
