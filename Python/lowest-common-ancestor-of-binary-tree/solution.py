c5b6d7622e8f00f0885c98db61ba50c81a29b7ac
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-10-17 13:50:51 -0500

    Added python solution for Lowest Common Ancestor of Binary Tree

---
+++ Python/lowest-common-ancestor-of-binary-tree/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+        
+        if root.val == p.val or root.val == q.val:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left is None:
+            return right
+        
+        if right is None:
+            return left
+        
+        return root
