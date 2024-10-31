69af0735017a9c53f2e381763d80f7cd22f89fe7
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2024-10-31 16:05:57 -0500

    Added java solution for Zero Array Transformation I

---
+++ Java/zero-array-transformation-I/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        for (int i = 0; i < queries.length; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+
+            for (int j = left; j <= right; j++) {
+
+                if (nums[j] != 0) {
+                    nums[j]--;
+                }
+
+                System.out.println(nums[j]);
+            }
+        }
+
+        for (int num : nums) {
+            if (num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
