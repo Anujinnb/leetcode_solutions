b694c9c178ac018294a75a5193f3bde3f9ef91eb
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2026-01-11 15:08:19 -0800

    Added complexity explanation for Count Residue Prefixes

--- Java/count-residue-prefixes/Solution.java
+++ Java/count-residue-prefixes/Solution.java
@@ -1,3 +1,6 @@
+//Time compleixty: O(n) because we traverse the string once 
+//Space complexity: O(1) because the HashSet size is limited to 26 characters, which is constant space.
+
 class Solution {
     public int residuePrefixes(String s) {
         HashSet<Character> set = new HashSet<>();
